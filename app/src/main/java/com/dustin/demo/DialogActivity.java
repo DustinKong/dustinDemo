c2caae2adb20c5c992e9466753142fd9f86ba2df
Melrose <qckong23@163.com>
2022-12-26 13:59:57 +0800

    dialog updated

--- app/src/main/java/com/dustin/demo/DialogActivity.java
+++ app/src/main/java/com/dustin/demo/DialogActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class DialogActivity extends AppCompatActivity {
     private Context mContext;
-    private Button dialog_btn1, dialog_btn2;
+    private Button dialog_btn1, dialog_btn2,dialog_btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +20,11 @@ public class DialogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dialog);
         dialog_btn1 = findViewById(R.id.dialog_btn1);
         dialog_btn2 = findViewById(R.id.dialog_btn2);
+        dialog_btn3 = findViewById(R.id.dialog_btn3);
         OnClick onClick = new OnClick();
         dialog_btn1.setOnClickListener(onClick);
         dialog_btn2.setOnClickListener(onClick);
+        dialog_btn3.setOnClickListener(onClick);
 
     }
 
@@ -49,6 +51,36 @@ public class DialogActivity extends AppCompatActivity {
 
                     break;
                 case R.id.dialog_btn2:
+                    final String[] array =new String[]{"男","女"};
+                    AlertDialog.Builder builder2 = new AlertDialog.Builder(DialogActivity.this);
+                    builder2.setTitle("选择性别").setSingleChoiceItems(array, 0, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(DialogActivity.this, array[i], Toast.LENGTH_SHORT).show();
+                            dialogInterface.dismiss();
+                        }
+                    }).setCancelable(false).show();
+                    break;
+                case R.id.dialog_btn3:
+                    final String[] array2=new String[]{"唱","跳","rap"};
+                    boolean[] isSelected = new boolean[]{false,false,false};
+                    AlertDialog.Builder builder3=new AlertDialog.Builder(DialogActivity.this);
+                    builder3.setTitle("选择兴趣").setMultiChoiceItems(array2, isSelected, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                            Toast.makeText(DialogActivity.this, array2[i]+":"+b, Toast.LENGTH_SHORT).show();
+                        }
+                    }).setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).setNegativeButton("no", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).show();
                     break;
             }
         }
